548f534f90ccfcb845a0afbdbadcfbc7ea521164
Fodor Levente <levilevi183@gmail.com>
2018-05-13 21:42:59 +0200

    Agent.java updated with new methods and refactoring

--- AgentGame/src/model/Agent.java
+++ AgentGame/src/model/Agent.java
@@ -1,9 +1,13 @@
 package model;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Random;
+import java.util.Scanner;
 
 import system.Client;
 import system.Server;
@@ -124,7 +128,8 @@ public class Agent {
 		this.agentCode = agentCode;
 
 		System.out.println(String.format("Ügynökség neve: " + agency.getName() + ". Ügynök kódja: " + agentCode));
-
+		readFromFile();
+		
 		System.out.println("Álnevek: ");
 		for (int i = 0; i < names.size(); i++) {
 			System.out.println("\t" + names.get(i));
@@ -139,7 +144,7 @@ public class Agent {
 		clientThread = new Thread(new Client(this));
 	}
 
-	public void threadStrart() {
+	public void threadStart() {
 		this.clientThread.start();
 		System.out.println(String.format(agency.getCode() + " kliens szál elindítva!"));
 
@@ -152,6 +157,55 @@ public class Agent {
 		return this.names.get(this.r.nextInt(this.names.size()));
 	}
 	
+	public boolean isSecreatAvailable() {
+		return this.secrets.containsValue(true);
+	}
 	
+	private String getFileName() {
+		return ("agent" + agency.getCode() + "-" + agentCode + ".txt");
+	}
+	
+	// Beolvasás fájlból readInfromation -> readFromFile
+	private void readFromFile() {
+		try (Scanner sc = new Scanner(new File(getFileName()));) {
+			this.names = Arrays.asList(sc.nextLine().split(" "));
+			this.secrets.put(sc.nextLine(), true);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+			System.out.println("Nem létezõ fájl!");
+			System.exit(1);
+		}
+	}
+	
+	public int getTipsForAgencyName(String name) {
+		int tips = 0;
+		for (int agencyCode: tipsOfAgencyCode.get(name).keySet()) {
+			if (tipsOfAgencyCode.get(name).get(agencyCode) == true) {
+				tips = agencyCode;
+			}
+		}
+		return tips;
+	}
+	
+	public boolean isCorrectTips(String name) {
+		boolean correct = false;
+		for (int agentCode: tipsOfAgentCode.get(name).keySet()) {
+			if (tipsOfAgentCode.get(name).get(agentCode) == true) {
+				correct = true;
+				break;
+			}
+		}
+		return correct;		
+	}
+	
+	public int getTipsForAgentName(String name) {
+		int tips = 0;
+		for (int agentCode: tipsOfAgentCode.get(name).keySet()) {
+			if (tipsOfAgentCode.get(name).get(agentCode) == true) {
+				tips = agentCode;
+			}
+		}
+		return tips;
+	}
 	
 }
